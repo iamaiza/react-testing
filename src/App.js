2ca3475268992496e70a461a030c058326e5f258
Aiza ali <aiza9382@gmail.com>
2024-01-08 12:11:45 +0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="form_container">
+        <form>
+          <input 
+            type="email" 
+            placeholder="Email Address"
+           />
+          <input
+            type="password" 
+            placeholder="Password" 
+          />
+          <input 
+            type="password" 
+            placeholder="Confirm Password" 
+          />
+          <button>Submit</button>
+        </form>
+      </div>
     </div>
   );
 }
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,53 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen } from "@testing-library/react";
+import App from "./App";
+import userEvent from "@testing-library/user-event";
 
-test('renders learn react link', () => {
+// test('renders learn react link', () => {
+//   render(<App />);
+//   const linkElement = screen.getByText(/learn react/i);
+//   expect(linkElement).toBeInTheDocument();
+// });
+
+test("inputs should be initially empty", () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const emailInput = screen.getByPlaceholderText(/email address/i);
+  const passwordInput = screen.getByPlaceholderText("Password")
+  const confirmPasswordInput = screen.getByPlaceholderText(/confirm password/i)
+  expect(emailInput.value).toBe("");
+  expect(passwordInput.value).toBe("")
+  expect(confirmPasswordInput.value).toBe("")
 });
+
+test("input should be able to type email", () => {
+  render(<App />);
+  const emailInput = screen.getByPlaceholderText(/email address/i);
+  userEvent.type(emailInput, "janeDoe22@gmail.com")
+  expect(emailInput.value).toBe("janeDoe22@gmail.com")
+})
+
+test("input should be able to type password", () => {
+  render(<App />);
+  const passwordInput = screen.getByPlaceholderText("Password")
+  userEvent.type(passwordInput, "janeDoe22@g")
+  expect(passwordInput.value).toBe("janeDoe22@g")
+})
+
+test("input should be able to type confirm password", () => {
+  render(<App />);
+  const confirmPasswordInput = screen.getByPlaceholderText(/confirm password/i)
+  userEvent.type(confirmPasswordInput, "janeDoe22@g")
+  expect(confirmPasswordInput.value).toBe("janeDoe22@g")
+})
+
+test("should show email error message on invalid email", () => {
+  render(<App />);
+  const emailInput = screen.getByPlaceholderText(/email address/i);
+  userEvent.type(emailInput, "janeDoe22gmail")
+
+  const submitBtn = screen.getByRole("button", {
+    name: /submit/i
+  })
+  userEvent.click(submitBtn)
+  const emailError = screen.getByText(/the email you input is invalid/i)
+  expect(emailError).toBeInTheDocument()
+})
